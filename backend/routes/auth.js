13662b672655a729023c473ed0233b9f24bac002
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2023-09-22 15:42:14 +0530

    added json web token for verification

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,24 +1,32 @@
 const express=require('express')
 const router=express.Router();
 const User=require('../models/User')
- 
+ const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken')
 
+
+const JWT_SECRET="Shripoojar"
 //Create user using  POST: "/api/auth". Doesnt require auth
 router.post('/createuser',async (req,res)=>{
 
     console.log(req.body);
-   
-    const user=User(req.body);
-    try{
-      await user.save().then(()=>{
-        res.status(200).json({
-          success:true
-        })
-        console.log("Successfuly saved");
+   const salt=  await bcrypt.genSalt(10) // generates a salt
+    secPass=  await bcrypt.hash(req.body.password,salt) 
 
-      })
+    // create user
+      
+    try
+    {
+       const user=await  User.create({
+        name:req.body.name,
+        password:secPass,
+        email:req.body.email
+       })
+        console.log("Successfuly saved");
+         
     }
-  catch(e){
+   catch(e)
+   {
     console.log(e);
     res.status(400).json({
       success:false,
@@ -26,6 +34,16 @@ router.post('/createuser',async (req,res)=>{
     })
      
   }
+
+
+  const user=  await User.findOne({name:req.body.name})
+  const data={
+    user:{
+      id:user.id
+    }
+  }
+  const authtoken=jwt.sign(data,JWT_SECRET)
+   res.json(authtoken);
      
 
 })
