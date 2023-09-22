0d075a3bff6045d181cd038edbb4d2c97865d38d
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2023-09-22 22:34:13 +0530

    authentication and log in using JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,40 @@
 const express=require('express')
 const router=express.Router();
 const User=require('../models/User')
- const bcrypt=require('bcryptjs');
+const bcrypt=require('bcryptjs');
 const jwt=require('jsonwebtoken')
-
+const emailValidator=require('email-validator');
+const fetchUser=require('../middleware/fetchUser')
 
 const JWT_SECRET="Shripoojar"
-//Create user using  POST: "/api/auth". Doesnt require auth
+//----------------------------------------------Create user using  POST: "/api/auth". Doesnt require auth--------------------------------------------
 router.post('/createuser',async (req,res)=>{
 
-    console.log(req.body);
+    const{name,email,password}=req.body;
+
+     console.log(req.body);
+
+    
+     // rejecting if user doesnt enter password or email or name
+
+    if(!name || !email || !password)
+    {
+        return res.status(400).json({
+            success:"false",
+            message:"Every Field is required"
+        })
+    }
+    // rejecting invalid email
+     var validEmail=emailValidator.validate(email);
+     if(!validEmail){
+        return res.status(400).json({
+            success:"false",
+            message:"Plzz provide validemail"
+        })
+     }
+     else
+     {
+
    const salt=  await bcrypt.genSalt(10) // generates a salt
     secPass=  await bcrypt.hash(req.body.password,salt) 
 
@@ -34,6 +59,8 @@ router.post('/createuser',async (req,res)=>{
     })
      
   }
+}
+// generating jwt token
 
 
   const user=  await User.findOne({name:req.body.name})
@@ -47,6 +74,78 @@ router.post('/createuser',async (req,res)=>{
      
 
 })
+
+
+//------------------------------------------------ Authenticate user using login :POST "/api/auth/login"-------------------------------------
+
+router.post('/login',async(req,res)=>{
+   const {email,password}=req.body;
+
+     // rejecting if user doesnt enter password or email
+        if(!email ||!password)
+        {
+          return res.status(400).json({
+            success:"false",
+            message:"Every Field is required"
+        })
+        }
+  
+
+        // rejecting if email is invalid
+        var validEmail=emailValidator.validate(email);
+        if(!validEmail){
+           return res.status(400).json({
+               success:"false",
+               message:"Plzz provide validemail"
+           })
+        }
+        try{
+          let user=await User.findOne({email})
+          if(!user)
+          {
+            return res.status(400).json({error:"Credentials are Not Mactching"})
+          }
+          const passwordCompare= await bcrypt.compare(password,user.password)
+          if(!passwordCompare)
+          {
+            return res.status(400).json({error:"Credentials are Not Mactching"})
+          }
+          const data={
+            user:{
+              id:user.id
+            }
+          }
+          const authtoken=jwt.sign(data,JWT_SECRET)
+           res.json(authtoken);
+        }  catch(e)
+        {
+         console.log(e);
+         res.status(500).json({
+           success:false,
+           message:e.message
+         })
+          
+       }
+
+})
+
+
+// getting logged in user details POST :/api/auth/getuser ---------------------------------------------------------------------------------------
+router.post('/getuser',fetchUser,async(req,res)=>
+{
+  try{
+   const userId=req.user.id;
+   const user=await User.findById(userId).select("-password")
+   res.send(user);
+  }
+  catch(e){
+    console.log(e.message);
+    res.status(500).send("Internal Sever Error")
+
+  }
+}
+ 
+)
 
 
 module.exports=router
